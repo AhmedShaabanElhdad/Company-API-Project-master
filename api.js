14d09258fe49830a3d365b9faf2374283fed16dc
Your Name <you@example.com>
2017-08-11 19:41:10 +0200

    adding logging and registration

--- api.js
+++ api.js
@@ -8,9 +8,25 @@ module.exports = function (express) {
     api.route('/login')
         .post(function (req, res) {
             var company = new Company({
-                name: req.body.name,
-                description: req.body.description,
-                Phone: req.body.Phone
+                UserPass: req.body.UserPass,
+                Email: req.body.Email
+            });
+            EndUsers.save(function (err) {
+                if (err) {
+                    res.sendStatus(406);
+                    return;
+                }
+                res.status(200).json({message: 'company has been created'});
+            });
+        });
+
+    api.route('/register')
+        .post(function (req, res) {
+            var company = new Company({
+                UserPass: req.body.UserPass,
+                UserPass: req.body.UserPass,
+                UserPass: req.body.UserPass,
+                Email: req.body.Email
             });
             EndUsers.save(function (err) {
                 if (err) {
